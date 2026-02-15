05e50393ee9b3936c79743c81c284d8a22b95478
Denis <s.1ned@yandex.ru>
2026-02-16 00:00:56 +0300

    update files_task

--- hw/files_task/files.py
+++ hw/files_task/files.py
@@ -1,4 +1,5 @@
 import os
+import json
 from pathlib import Path
 
 # 1)Создать текстовый файл и записать в него 6 строк.
@@ -73,3 +74,56 @@ from pathlib import Path
 #             file_lst.append(line + "!")
 
 # print(file_lst)
+
+
+# 6)В справочной аэропорта хранится расписание вылета
+# самолетов на следующие сутки. Для каждого рейса указаны
+# номер рейса, пункт назначения, время вылета. 
+# Вывести все номера рейсов и время вылета самолета для 
+# заданного пункта назначения.
+
+# path = "hw/files_task/flights.json"
+# mode = "r"
+# encode = "utf-8"
+
+# with open(file=path, mode=mode, encoding=encode) as file:
+#     flights = json.load(file)
+
+# found_flight = False
+# flight_destination = input("Введите пункт назначения: ")
+
+
+# for flight in flights:
+#     if flight["destination"] == flight_destination:
+#         print(f"{flight["flight_number"]} {flight["departure_time"]}")
+#         found_flight = True
+    
+# if not found_flight:
+#     print("Рейс не найден")
+
+
+# 7)Ведомость студентов, сдававших сессию, содержит ФИО и оценки по 
+# четырем предметам. Вывести список студентов, сдавших сессию со 
+# средним баллом больше 7.
+
+# path = "hw/files_task/student_grades.json"
+# mode = "r"
+# encode = "utf-8"
+
+# with open(file=path, mode=mode, encoding=encode) as file:
+#     students = json.load(file)
+
+# for student in students:
+#     grades = student["grades"]
+#     avg_grades = sum(grades) / 4
+#     if avg_grades > 7:
+#         print(f"{student['full_name']} — средний балл: {avg_grades}")
+
+
+# 8)Вашей задачей будет восстановление исходной строки обратно. 
+# Напишите программу, которая считывает из файла строку, 
+# соответствующую тексту, сжатому с помощью кодирования повторов, 
+# и производит обратную операцию, получая исходный текст. 
+# Для решение вам необходимо открыть файл для чтения 8.txt
+
+
